4ba419d31fbfd5e3378022881d52e59fd4c261b9
Wendel <138839125+Wen-nunes@users.noreply.github.com>
2025-05-06 22:19:49 -0300

    Delete Avaliacao directory

--- Avaliacao/src/main/java/com/example/avaliacao/repository/VendaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.avaliacao.repository;
-
-import com.example.avaliacao.model.Vendas;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VendaRepository extends JpaRepository<Vendas, Integer> {
-} 
