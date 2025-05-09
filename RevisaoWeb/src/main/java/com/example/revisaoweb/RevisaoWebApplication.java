e79436fd11f46b5d2d4d65a9a16841d44908da9f
Wendel <138839125+Wen-nunes@users.noreply.github.com>
2025-05-08 21:56:55 -0300

    Add files via upload

---
+++ RevisaoWeb/src/main/java/com/example/revisaoweb/RevisaoWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.revisaoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevisaoWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RevisaoWebApplication.class, args);
+    }
+
+}
